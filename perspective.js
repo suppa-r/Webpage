bab747e3aabe8ee46863ca0ed3ec465ecf552d6f
suppa-r <supparaffaele43@gmail.com>
2024-11-12 22:02:03 -0500

    aaa

--- perspective.js
+++ perspective.js
@@ -1,4 +1,6 @@
-
+/*********
+ * made by Matthias Hurrle (@atzedent)
+ */
 let editMode = false // set to false to hide the code editor on load
 let resolution = .5 // set 1 for full resolution or to .5 to start with half resolution on load
 let renderDelay = 1000 // delay in ms before rendering the shader after a change
